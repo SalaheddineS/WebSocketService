cc765f6728b19a052d39639468b28bef263a97f9
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-05-03 14:41:53 +0100

    Added Initial web socket Handlers

--- src/main/java/com/WebSocketService/Configuration/WebSocketConfiguration.java
+++ src/main/java/com/WebSocketService/Configuration/WebSocketConfiguration.java
@@ -1,6 +1,9 @@
 package com.WebSocketService.Configuration;
 
+import com.WebSocketService.Handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -8,8 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
+    private static final String CHAT_ENDPOINT = "/chat";
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry handler) {
+        handler.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT).setAllowedOrigins();
+    }
+
+    @Bean
+    public WebSocketHandler getChatWebSocketHandler() {
+        return new ChatWebSocketHandler();
     }
 }
